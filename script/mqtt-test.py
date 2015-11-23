5cfa5cedc85e6dd181d3f781a08f76344b1c713f
Xiaoying Pu <xp002@bucknell.edu>
2015-11-22 20:43:31 -0500

    scripts

---
+++ script/mqtt-test.py
@@ -0,0 +1,24 @@
+# Monitors all topics on the network
+# Fake n_back client to send PAUSE message
+# Could persist the messages to a csv (flyloop does that too)
+
+import threading
+from paho.mqtt.client import Client
+from paho.mqtt.publish import single
+import csv
+import time
+
+
+# sending fake pauses emulating the n-back calibrator
+
+
+def on_message(mqttc, obj, msg):
+	print(msg.payload)
+
+if __name__ == "__main__":
+	client = Client()
+	client.on_message = on_message
+	#client.connect("broker.mqttdashboard.com")
+	client.connect("mqtt.bucknell.edu")
+	client.subscribe("#")
+	client.loop_forever()
